67e62b9801d7f300e1b27a36577c9187b796c765
Albert Dugba <albert.dugba@gmail.com>
2019-09-03 15:50:12 +0000

    code refactoring

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -2,8 +2,23 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 import { ProductConsumer } from "./Context";
+import { shopProducts } from "./Data";
 
 class ProductDetails extends Component {
+  // state = {
+  //   selectedProduct: ""
+  // };
+
+  // componentDidMount() {
+  //   const products = shopProducts.find(product => {
+  //     if (product.id === this.props.match.params.id) {
+  //       console.log("Product Page", product.price);
+  //     }
+  //   });
+
+  //   this.setState({ selectedProduct: products });
+  // }
+
   render() {
     return (
       <ProductPage>
@@ -12,7 +27,14 @@ class ProductDetails extends Component {
             const { products } = value;
             products.find(product => {
               if (product.id === this.props.match.params.id) {
-                return <div className="page">{console.log(product.name)}</div>;
+                return (
+                  <div className="page">
+                    {console.log(product.name)}
+
+                    <h1>Product Name: {console.log(product.name)}</h1>
+                    {/* This doesnt also show in the DOM but logs to the console */}
+                  </div>
+                );
               }
             });
           }}
